0dbb323a1b49520b97848947cc19c77758e7827e
Gustavo Biolcatti <43222626+gustavobiolcatti@users.noreply.github.com>
2022-05-04 17:41:39 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header")
+
+window.onwheel = e => {
+    if(e.deltaY >= 0){
+        // Scroll-down
+        header.classList.remove("header--show")
+    } else {
+        // Scroll-up
+        header.classList.add("header--show")
+    }
+  }
