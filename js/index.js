651751d8ba8c8f5206e2a46f3af82e38989cfbc0
Gustavo Gabriel Pereira Biolcatti <43222626+GustavoBiolcatti@users.noreply.github.com>
2022-05-07 10:42:34 -0300

    refatoração de código

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 const header = document.querySelector("header")
 
 window.onwheel = e => {
-    if(e.deltaY >= 0){
+    if(e.deltaY >= 0) {
         // Scroll-down
         header.classList.remove("header--show")
-    } else {
+    }
+    else {
         // Scroll-up
         header.classList.add("header--show")
     }
